9087c803d74db7614a8c0e310d282fd7189c183e
ThomasAngelo <tangelo930@gmail.com>
2015-03-28 00:35:05 -0400

    Login & Home
    Login page being built. Home page added. Routing Works.

--- src/app/app.js
+++ src/app/app.js
@@ -7,6 +7,11 @@ TandemApp.config(function($routeProvider)
 		{
 			templateUrl: 'app/pages/login.html',
 			controller: 'loginController'
+		})
+		.when('/home',
+		{
+			templateUrl: 'app/pages/home.html',
+			controller: 'homeController'
 		});
 });
 
@@ -14,5 +19,12 @@ TandemApp.controller('loginController', ['$scope',
 	function($scope)
 	{
 		$scope.test = "login";
+	}
+	]);
+	
+TandemApp.controller('homeController', ['$scope',
+	function($scope)
+	{
+		$scope.test = "home";
 	}
 	]);
